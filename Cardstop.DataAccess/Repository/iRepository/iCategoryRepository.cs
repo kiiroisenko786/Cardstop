3b57b3852a382f6c259d137b6669c0ca0f4df648
Taahaa <58344496+JJosuke@users.noreply.github.com>
2023-10-06 01:51:11 +0100

    implement category repository

---
+++ Cardstop.DataAccess/Repository/iRepository/iCategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Cardstop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cardstop.DataAccess.Repository.iRepository
+{
+    public interface iCategoryRepository : iRepository<Category>
+    {
+        void Update(Category category);
+        void Save()
+    }
+}
