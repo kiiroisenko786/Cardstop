ad71ec9d60d337e4b0141650e06a81e8670744e3
JJosuke <projectyuni5@gmail.com>
2023-10-14 01:35:17 +0100

    configurating product model

---
+++ Cardstop.Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cardstop.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Effect { get; set; }
+
+    }
+}
