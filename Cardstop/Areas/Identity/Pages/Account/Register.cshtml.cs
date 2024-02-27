8a05227ca4213d91f0a2fce14d4d580fb73aeb98
Taahaa <58344496+JJosuke@users.noreply.github.com>
2024-02-27 13:45:06 +0000

    register company user -> fix register page UI

--- Cardstop/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Cardstop/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -173,6 +173,12 @@ namespace Cardstop.Areas.Identity.Pages.Account
                 user.State = Input.State;
                 user.PostZipCode = Input.PostZipCode;
                 user.PhoneNumber = Input.PhoneNumber;
+
+                if(Input.Role == SD.Role_Company)
+                {
+                    user.CompanyId = Input.CompanyId;
+                }
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
--- Cardstop/obj/Debug/net8.0/Cardstop.AssemblyInfo.cs
+++ Cardstop/obj/Debug/net8.0/Cardstop.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cardstop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd5943380ff86a944b0541f884d1f831546333ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2233deb60fc7ee2fb0da282079025c8efa4dac44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cardstop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cardstop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
